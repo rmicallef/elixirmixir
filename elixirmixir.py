5db0b020fc360dfc35f0d07edd74be8526d2b977
Ryan Micallef <micallef@gmail.com>
2019-08-16 10:09:36 -0400

    first commit

---
+++ elixirmixir.py
@@ -0,0 +1,53 @@
+import time
+#import RPi.GPIO as GPIO
+
+SECONDS_PER_ML = 0.1250
+relayPins = [21, 22, 23, 24, 25, 26]
+essenceNames = ["Almond", "Birch", "Cheddar", "Dandelion", "Earwig", "Fluorine"]
+
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(relayPins, GPIO.OUT, initial=GPIO.LOW)
+
+print("Pins:", relayPins)
+print("essenceNames:", essenceNames)
+
+
+def mixElixir(essenceRecipe):
+
+  print("Recipe:", essenceRecipe)
+
+  remainingRecipe = normalizeRecipe(essenceRecipe, 100)
+
+  while sum(i > 0 for i in remainingRecipe):
+
+    mixTick(remainingRecipe)
+    remainingRecipe = [max(0, x-1) for x in remainingRecipe]
+
+  # Turn all pumps off
+  #GPIO.output(relayPins, GPIO.LOW)
+  #GPIO.cleanup() # CONFIRM BEHAVIOR HERE - WILL THIS PUT RELAYS IN A WEIRD STATE?
+
+  return "Elixir mixed."
+
+
+def mixTick(remainingRecipe):
+
+  #print("MixTick:", remainingRecipe)
+
+  for i in range(len(remainingRecipe)):
+
+    if remainingRecipe[i] > 0:
+      #GPIO.output(relayPins[i], GPIO.HIGH)
+      #print(essenceNames[i], "is on.")
+      time.sleep(0.0001) # PLACEHOLDER
+
+    else:
+      #GPIO.output(relayPins[i], GPIO.LOW)
+      #print(essenceNames[i], "is off.")
+      time.sleep(0.0001) # PLACEHOLDER
+
+  time.sleep(SECONDS_PER_ML)
+
+
+def normalizeRecipe(recipe, total):  
+  return [((essence*total)/sum(recipe)) for essence in recipe]
